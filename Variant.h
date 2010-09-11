be11562045cf92fc4d0abf3f14b0b262d1a20796
Jakub Wieczorek <fawek@fawek.net>
2010-09-11 17:38:55 +0200

    Remove the custom reference counting in Variant and use shared_ptr from the TR1.

--- Variant.h
+++ Variant.h
@@ -20,39 +20,34 @@
 
 #include <cassert>
 #include <typeinfo>
-
-// FIXME: Make this thread-safe. Likely at the cost of reference counting.
+#include <tr1/memory>
 
 class Variant {
 public:
     Variant()
-        : m_data(0)
+        : m_data()
     {
     }
 
     Variant(const Variant& variant)
         : m_data(variant.m_data)
     {
-        if (m_data)
-            dataRef();
     }
 
     template <typename Type>
     Variant(const Type& value)
-        : m_data(0)
+        : m_data()
     {
         set(value);
     }
     
     ~Variant()
     {
-        if (m_data)
-            dataDeref();
     }
 
     bool isNull() const
     {
-        return !m_data;
+        return !m_data.get();
     }
     
     template <typename Type>
@@ -72,8 +67,7 @@ public:
 
     void reset()
     {
-        if (m_data)
-            dataDeref();
+        m_data.reset();
     }
 
     template <typename Type>
@@ -82,34 +76,24 @@ public:
         if (!is<Type>())
             return Type();
         
-        Data<Type>* data = static_cast<Data<Type>*>(m_data);
+        Data<Type>* data = static_cast<Data<Type>*>(m_data.get());
         return data->value();
     }
 
     template <typename Type>
     void set(const Type& value)
     {
-        if (is<Type>() && m_data->isMutable()) {
-            Data<Type>* data = static_cast<Data<Type>*>(m_data);
+        if (m_data && m_data->type() == typeid(Type) && m_data.unique()) {
+            Data<Type>* data = static_cast<Data<Type>*>(m_data.get());
             data->setValue(value);
         } else {
-            if (m_data)
-                dataDeref();
-            m_data = new Data<Type>(value);
-            dataRef();
+            m_data = std::tr1::shared_ptr<AbstractData>(new Data<Type>(value));
         }
     }
 
     Variant& operator=(const Variant& variant)
     {
-        if (m_data == variant.m_data)
-            return *this;
-        
-        if (m_data)
-            dataDeref();
         m_data = variant.m_data;
-        if (m_data)
-            dataRef();
         return *this;
     }
 
@@ -119,7 +103,7 @@ public:
             return true;
         if (!m_data || !variant.m_data)
             return false;
-        return type() == variant.type() && m_data->valueEquals(variant.m_data);
+        return type() == variant.type() && m_data->valueEquals(variant.m_data.get());
     }
 
     bool operator!=(const Variant& variant) const
@@ -128,42 +112,14 @@ public:
     }
 
 private:
-   class AbstractData {
+    class AbstractData {
     public:
-        AbstractData()
-            : m_refCount(0)
-        {
-        }
-
         virtual ~AbstractData()
         {
         }
-
-        void ref()
-        {
-            ++m_refCount;
-            assert(m_refCount > 0);
-        }
-
-        bool deref()
-        {
-            assert(m_refCount > 0);
-            --m_refCount;
-            return m_refCount == 0;
-        }
-
-        bool isMutable() const
-        {
-            assert(m_refCount > 0);
-            return m_refCount == 1;
-        }
-
+        
         virtual const std::type_info& type() const = 0;
-
         virtual bool valueEquals(AbstractData* abstractData) const = 0;
-
-    protected:
-        int m_refCount;
     };
 
     template <typename Type>
@@ -201,21 +157,7 @@ private:
         Type m_value;
     };
 
-    void dataRef()
-    {
-        assert(m_data);
-        m_data->ref();
-    }
-    
-    void dataDeref()
-    {
-        assert(m_data);
-        if (m_data->deref())
-            delete m_data;
-        m_data = 0;
-    }
-
-    AbstractData* m_data;
+    std::tr1::shared_ptr<AbstractData> m_data;
 };
 
 #endif	/* _VARIANT_H */
